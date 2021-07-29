c3ad14d3c4ff8c5e7ca5fbc9950726dde34ebd65
Shivam Sheth <shivam@shethmail.co.uk>
2021-07-29 10:48:09 +0100

    1.0 - Finished exercise

--- app.js
+++ app.js
@@ -5,41 +5,49 @@ const port = 3000
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
 
-const Battle = require('./src/battle.js')
-const Player = require('./src/player.js')
-const battle = new Battle()
+// const Battle = require('./src/battle.js')
+// const Player = require('./src/player.js')
+// const battle = new Battle()
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+const indexRouter = require('./routes/index.js')
+const gameRouter = require('./routes/game.js')
+const turnRouter = require('./routes/turn.js')
 
-app.post('/game', (req, res) => {
-  const names = [req.body.player1, req.body.player2]
-  battle.setup(names)
+app.use('/', indexRouter)
+app.use('/game', gameRouter)
+app.use('/turn', turnRouter)
 
-  res.redirect('/game')
-})
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
 
-app.get('/game', (req, res) => {
-  const player = battle.currentPlayer()
+// app.post('/game', (req, res) => {
+//   const names = [req.body.player1, req.body.player2]
+//   battle.setup(names)
 
-  res.render('game', {
-    name: player.name,
-    health: player.health
-  })
-})
+//   res.redirect('/game')
+// })
 
-app.post('/turn', (req, res) => {
-  battle.turn()
-  const currentPlayer = battle.currentPlayer()
-  const otherPlayer = battle.otherPlayer()
-  battle.switch()
+// app.get('/game', (req, res) => {
+//   const player = battle.currentPlayer()
 
-  res.render('turn', {
-    currentPlayer: currentPlayer,
-    otherPlayer: otherPlayer
-  })
-})
+//   res.render('game', {
+//     name: player.name,
+//     health: player.health
+//   })
+// })
+
+// app.post('/turn', (req, res) => {
+//   battle.turn()
+//   const currentPlayer = battle.currentPlayer()
+//   const otherPlayer = battle.otherPlayer()
+//   battle.switch()
+
+//   res.render('turn', {
+//     currentPlayer: currentPlayer,
+//     otherPlayer: otherPlayer
+//   })
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ routes/game.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const Battle = require('../src/battle.js')
+const Player = require('../src/player.js')
+
+router.post('/', (req, res) => {
+  const battle = new Battle()
+  const names = [req.body.player1, req.body.player2]
+  battle.setup(names)
+  req.app.locals.battle = battle
+
+  res.redirect('/game')
+})
+
+router.get('/', (req, res) => {
+    const player = req.app.locals.battle.currentPlayer()
+  
+    res.render('game', {
+      name: player.name,
+      health: player.health
+    })
+  })
+
+  module.exports = router
